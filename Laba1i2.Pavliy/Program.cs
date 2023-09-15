93465f31c004b5f37ec1c17cdd44587db4935f30
user <user@DESKTOP-TCJCO3Q>
2023-09-15 12:05:42 +0200

    Добавьте файлы проекта.

---
+++ Laba1i2.Pavliy/Program.cs
@@ -0,0 +1,18 @@
+﻿//Выс.уровень   
+//Console.Write("Ведите y:");
+//double y = double.Parse(Console.ReadLine());
+//Console.Write("Ведите x:");
+//double x = double.Parse(Console.ReadLine());
+//Console.Write("Ведите d:");
+//double d = double.Parse(Console.ReadLine());
+//double R = (Math.Pow(Math.Cos(y), 3) + Math.Pow(2, x) * d) / (Math.Exp(y) + Math.Log(Math.E, (Math.Pow(Math.Sin(x + 7.4), 2))));
+//Console.WriteLine($"R = {R:F2}");
+
+//Выс.уровень
+Console.Write("Ведите a:");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Ведите b:");
+double b = double.Parse(Console.ReadLine());
+double c = 12;
+double x = (((c - b) * a) / 2);
+Console.WriteLine($"x = {x:F2}");
